8949f609f9a3ddb7530e86029affb48e81dce99a
iiot-tw <32183809+iiot-tw@users.noreply.github.com>
2019-09-20 11:16:36 +0800

    correction
    fix the line trimmed in previous committing

--- sysfspins.sh
+++ sysfspins.sh
@@ -4,4 +4,4 @@ echo File igt$1 not found
 exit 1
 fi
 
-cat `which igt$1` | grep ')\s*PIN=' | sed 's/)\s*PIN=/:/g' | sed 's/\s//g' | se$
+cat `which igt$1` | grep ')\s*PIN=' | sed 's/)\s*PIN=/:/g' | sed 's/\s//g' | sed 's/;//g'
