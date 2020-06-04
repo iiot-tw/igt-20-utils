87b87e93958f9c1868d84a7a7ea9c652d8b5872e
iiot-tw <32183809+iiot-tw@users.noreply.github.com>
2020-06-04 15:20:40 +0800

    add btn poll (not yet done)

--- igt-sdk.py
+++ igt-sdk.py
@@ -1,3 +1,5 @@
+import select
+
 class IGT():
 
   __led_fs = ""
@@ -18,6 +20,28 @@ class IGT():
     ret=f.read()
     f.close()
     return ret
+
+  def btn_create_edge(self, i, edge):
+    if i<0 or i>2:
+      return -1
+
+    gpio = self.__btn_gpio
+
+    f = open(self.__gpio_fs+gpio[i]+"/edge", "w")
+    f.write(edge)
+    f.close
+
+    f = open(self.__gpio_fs+gpio[i]+"/value", "r")
+
+    return f
+
+  def btn_wait_edge(self, f, timeout):
+    po = select.poll()
+    po.register(f, select.POLLPRI)
+
+    events = po.poll(timeout)
+
+    return events
 
   def led_write(self, i, v):
     if i<0 or i>5:
@@ -53,9 +77,22 @@ if __name__== '__main__':
 
   a=IGT30()
 
+  f=a.btn_create_edge(0, 'both')
+
   while True:
+    events = a.btn_wait_edge(f, 5000)
+    if not events:
+        print ('.')
+    else:
+        f.seek(0)
+        state_last = f.read()
+        print ('Val: %s' % state_last)
+
+
+"""while True:
     a.led(4,1)
     time.sleep(1)
     a.led(4,0)
     time.sleep(1)
-    print (a.btn_read(0))
+    print (a.btn_read(0))"""
+
