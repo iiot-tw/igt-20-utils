d33c10e22b95c2040528744eac03259f57b67368
iiot-tw <32183809+iiot-tw@users.noreply.github.com>
2020-06-15 18:03:14 +0800

    Create igt-20-dialer.sh

---
+++ igt-20-dialer.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+if [ "x$1" == "x" ]; then
+  echo "mdial: Please specify Modem"
+  exit 1
+fi
+
+  for i in {1..30}
+  do
+    echo "igt-dialer: check modem: $i"
+    [ -e $1 ] && break
+    sleep 1
+  done
+  [ $i = 30 ] && ( echo "igt-dialer: modem not found" ; exit 2 )
+
+  while [ 1 ];
+  do
+    echo "igt-dialer: dialing"
+    #at_init.py if for ublox-L210
+    #python3 /neousys/at_init.py
+    igt20 led1 1
+# set Auto Reconnect = off in wvdial.conf    
+    wvdial
+    echo "igt-dialer: wait for redial"
+    igt20 led1 0
+    sleep 10
+  done
