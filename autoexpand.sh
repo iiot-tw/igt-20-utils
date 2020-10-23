bad0670ef13d0d446502f045018e9d8a4541df2d
iiot-tw <32183809+iiot-tw@users.noreply.github.com>
2020-10-23 19:22:18 +0800

    Update autoexpand.sh

--- autoexpand.sh
+++ autoexpand.sh
@@ -1,5 +1,6 @@
 wget https://raw.githubusercontent.com/iiot-tw/igt-20-utils/master/grow_partition.sh
-wget -O /neousys/generic-startup.sh https://raw.githubusercontent.com/iiot-tw/igt-20-utils/master/neousys/generic-startup.sh
+#newer image seem modified generic-startup.sh
+#wget -O /neousys/generic-startup.sh https://raw.githubusercontent.com/iiot-tw/igt-20-utils/master/neousys/generic-startup.sh
 chmod +x grow_partition.sh
 ./grow_partition.sh
 reboot
