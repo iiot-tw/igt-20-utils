d63fab44d61fc7a5ac08b8ba5c54bc48c4b31347
iiot-tw <32183809+iiot-tw@users.noreply.github.com>
2021-01-13 10:36:50 +0800

    add disclaimer

--- igtutil-eth0dhcp.sh
+++ igtutil-eth0dhcp.sh
@@ -1,4 +1,15 @@
-# works only when eth0 is configured in /etc/network/interfaces
-# note that some network manager, such as connman, will ignore configuration in /etc/network/interfaces
+# This script is created as an EXAMPLE for specific purpose on Neousys IGT series.
+#
+# It works only when eth0 is configured in /etc/network/interfaces
+# Note that some network manager, such as connman, will ignore the configuration
+# in /etc/network/interface and make this script not work as expected.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
+# NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
+# NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE AUTHORS SHALL NOT, UNDER ANY
+# CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
+# DAMAGES, FOR ANY REASON WHATSOEVER.
+#
 sed -i 's|iface eth0 inet static|iface eth0 inet dhcp|' /etc/network/interfaces
 systemctl restart networking
