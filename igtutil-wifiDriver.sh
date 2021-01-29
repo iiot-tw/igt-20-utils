67950bdca1e613db1ac44b8d2bcaf6f11deffd80
iiot-tw <32183809+iiot-tw@users.noreply.github.com>
2021-01-29 10:29:57 +0800

    Update igtutil-wifiDriver.sh

--- igtutil-wifiDriver.sh
+++ igtutil-wifiDriver.sh
@@ -3,3 +3,7 @@
 cp ./88x2bu.ko.xz  /lib/modules/$(uname -r)
 cp ./88x2bu.conf /etc/modprobe.d/
 depmod
+modprobe 88x2bu
+
+## the following script install BLE firmware for some WiFi BLE combo card
+# apt update && apt install firmware-realtek
