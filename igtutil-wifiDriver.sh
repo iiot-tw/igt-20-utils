a0faf4f7d4dce1fae0847ae0791792e9cc76a968
iiot-tw <32183809+iiot-tw@users.noreply.github.com>
2021-01-29 10:22:22 +0800

    Update igtutil-wifiDriver.sh

--- igtutil-wifiDriver.sh
+++ igtutil-wifiDriver.sh
@@ -1,2 +1,5 @@
+# Not all mPCIe WiFi cards works on IGT series.
+# this script works for specific models only.
 cp ./88x2bu.ko.xz  /lib/modules/$(uname -r)
 cp ./88x2bu.conf /etc/modprobe.d/
+depmod
