a902d0760acbf2533a85c431e19f71fe33e29ad5
iiot-tw <32183809+iiot-tw@users.noreply.github.com>
2021-09-08 12:10:25 +0800

    Create igtutil-ttyTest.sh

---
+++ igtutil-ttyTest.sh
@@ -0,0 +1,66 @@
+# This script is created as an EXAMPLE for specific purpose on Neousys IGT series.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
+# NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
+# NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE AUTHORS SHALL NOT, UNDER ANY
+# CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
+# DAMAGES, FOR ANY REASON WHATSOEVER.
+#
+#!/bin/bash
+
+if [ "x$1" != "x/dev/ttyS2" ]; then
+  echo "Usage: $0 /dev/ttyS2 115200 <string_to_send>"
+  echo "Please specify correct device."
+  exit 1
+fi
+
+if [ "x$2" != "x115200" ]; then
+  echo "Usage: $0 /dev/ttyS2 115200 <string_to_send>"
+  echo "Please specify correct baud rate."
+  exit 2
+fi
+
+if [ "x$3" = "x" ]; then
+  echo "Usage: $0 /dev/ttyS2 115200 <string_to_send>"
+  echo "Please specify a string to send"
+  exit 3
+fi
+
+device=$1
+speed=$2
+message=$3
+
+dumpFile=/dev/shm/ttydump
+stty -F $device $speed raw -echo -crtscts
+
+exec 3<$device
+
+run=true;
+count=0;
+
+while $run;
+do
+  cat <&3 > $dumpFile &
+  PID=$!
+  echo "Message sent: $message"
+  echo $message > $device
+  sleep 0.2s
+  kill $PID
+  wait $PID 2>/dev/null
+
+  response=$(<$dumpFile)
+  echo "Response rcv: $response"
+
+  count=$((count+1));
+
+  if [ "x$message" != "x$response" ]; then
+    echo "$count: FAILED!!"
+    run=false;
+  else
+    echo "$count: PASSED!!"
+    run=false;  #remark this line to enable an infinite test
+  fi
+done
+
+exec 3<&-
