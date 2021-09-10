0885e154cf479b5e110fbd002e8cd74b854c1cbd
iiot-tw <32183809+iiot-tw@users.noreply.github.com>
2021-09-10 14:47:37 +0800

    add AT command test
    Messages start with "at+", small letter, will be treated as an AT command. No trailing \r required. Tested on SIM7600SA

--- igtutil-ttyLoopbackTest.sh
+++ igtutil-ttyLoopbackTest.sh
@@ -18,13 +18,13 @@ trap finish SIGINT
 if [ "x$1" != "x/dev/ttyS2" ]; then
   echo "Usage: $0 /dev/ttyS2 115200 <string_to_send>"
   echo "Please specify correct device."
-  exit 1
+#  exit 1
 fi
 
 if [ "x$2" != "x115200" ]; then
   echo "Usage: $0 /dev/ttyS2 115200 <string_to_send>"
   echo "Please specify correct baud rate."
-  exit 2
+#  exit 2
 fi
 
 if [ "x$3" = "x" ]; then
@@ -43,13 +43,21 @@ stty -F $device $speed raw -echo
 
 exec 3<$device
 
-run=true;
-count=1;
+run=true
+count=1
+timeout=0
+atcmd=false
 
 if [ "x$message" = "xechoback" ]; then
   echo "Running in Echoback server mode:"
   echo "device: $device"
   echo "speed:  $speed"
+elif [ "x${message:0:3}" = "xat+" ]; then
+  echo "AT Command Detected"
+  echo -ne "Message sent: $message\n"
+  echo -ne "$message\r" > $device
+  atcmd=true
+  sleep 0.1s
 else
   echo "Message sent: $message"
   echo $message > $device
@@ -73,15 +81,25 @@ do
   elif [ "x$message" = "xechoback" ]; then
     echo $count: $response
     echo $response > $device
+
     count=$((count+1));
   else
     timeout=0
+    if [ $atcmd = true ]; then
+      echo "READ AT Command RESPONSE"
+      read -t1 response <&3 #$device
+    fi
+
     echo "Message recv: $response"
-    if [ "x$message" = "x$response" ]; then
-      echo "$count: PASSED!!"
-      #run=false;  #remark this line to enable an infinite test
+    if [ $atcmd = false ]; then
+      if [ "x$message" = "x$response" ]; then
+        echo "$count: PASSED!!"
+        #run=false;  #remark this line to enable an infinite test
+      else
+        echo "$count: FAILED!!"
+        run=false;
+      fi
     else
-      echo "$count: FAILED!!"
       run=false;
     fi
   fi
