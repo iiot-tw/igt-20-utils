d58d44ff66cc89cfbd4308de40c9b4e40174ed16
iiot-tw <32183809+iiot-tw@users.noreply.github.com>
2021-09-23 12:07:44 +0800

    add build command

--- igt-33_read_volt.c
+++ igt-33_read_volt.c
@@ -1,9 +1,24 @@
+/* This script is created as an EXAMPLE for specific purpose on Neousys IGT series.
+
+   It works only when eth0 is configured in /etc/network/interfaces
+   Note that some network manager, such as connman, will ignore the configuration
+   in /etc/network/interface and make this script not work as expected.
+
+   THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
+   NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
+   NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE AUTHORS SHALL NOT, UNDER ANY
+   CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
+   DAMAGES, FOR ANY REASON WHATSOEVER.
+
+built with: gcc igt-33_read_volt.c -o igt-33_read_volt -l igt
+*/
 #include <stdint.h>
 #include <stdio.h>
 #include <igt.h>
 #include <unistd.h>
 
-#define AI_CH   1
+#define AI_CH   0
 
 int main(void) {
     double fAI0;
