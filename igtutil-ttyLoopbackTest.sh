f47750465ec6de5e4b61593b097107fac2a020e9
iiot-tw <32183809+iiot-tw@users.noreply.github.com>
2021-09-29 09:23:38 +0800

    remove echoback mode to simplify the program
    "cat /dev/ttySx | tee /dev/ttySx" could do something similar

--- igtutil-ttyLoopbackTest.sh
+++ igtutil-ttyLoopbackTest.sh
@@ -16,21 +16,27 @@ function finish() {
 trap finish SIGINT
 
 if [ "x$1" != "x/dev/ttyS2" ]; then
-  echo "Usage: $0 /dev/ttyS2 115200 <string_to_send>"
+  echo "Usage: $0 /dev/ttyS2 115200 <string_to_send> [last]"
   echo "Please specify correct device."
 #  exit 1
 fi
 
 if [ "x$2" != "x115200" ]; then
-  echo "Usage: $0 /dev/ttyS2 115200 <string_to_send>"
+  echo "Usage: $0 /dev/ttyS2 115200 <string_to_send> [last]"
   echo "Please specify correct baud rate."
 #  exit 2
 fi
 
 if [ "x$3" = "x" ]; then
-  echo "Usage: $0 /dev/ttyS2 115200 <string_to_send>"
+  echo "Usage: $0 /dev/ttyS2 115200 <string_to_send> [last]"
   echo "Please specify a string to send"
   exit 3
+fi
+
+if [ "x$4" != "x" ]; then
+    longtest=true
+else
+    longtest=false
 fi
 
 device=$1
@@ -44,74 +50,69 @@ stty -F $device $speed raw -echo
 exec 3<$device
 
 run=true
-count=1
-timeout=0
+count=0
 atcmd=false
 server=false
+failcount=0
+timeout_max=5
 
-if [ "x$message" = "xechoback" ]; then
-  server=true
-  echo "Running in Echoback server mode:"
-  echo "device: $device"
-  echo "speed:  $speed"
-elif [ "x${message:0:3}" = "xat+" ]; then
-  echo "AT Command Detected"
-  echo -ne "Message sent: $message\n"
-  echo -ne "$message\r" > $device
-  atcmd=true
-  sleep 0.1s
-else
-  echo "Message sent: $message"
-  echo $message > $device
-  sleep 0.1s
-fi
+clear
 
+SECONDS=0
 
 while $run;
 do
-  read -t1 response <&3 #$device
-  if [ "x$response" = "x" ]; then
+  if [ $longtest = true ]; then
+    duration=$SECONDS
+    echo -e "\033[1;1f$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."  
+  fi
+  if [ "x${message:0:3}" = "xat+" ]; then
+    echo "AT Command Detected"
+    echo -ne "Message sent: $message\n"
+    echo -ne "$message\r" > $device
+    atcmd=true
+    #sleep 0.1s
+  else
+    echo "Message sent: $message"
+    echo $message > $device
+    #sleep 0.1s
+  fi
+  count=$(($count+1))
+
+  timeout=0
+  response=""
+  
+  while [ $timeout -lt $timeout_max ] && [ "x$response" == "x" ];
+  do
+    #if [ $run = false ]; then break; fi
+    read -t1 response <&3 #$device
     timeout=$((timeout+1))
     echo -n .
-    if [ $timeout -gt 10 ] && [ $server = false ]; then
-       run=false
-    fi
-    continue
-  fi
+  done
   
   if [ $atcmd = true ]; then
     echo "READ AT Command RESPONSE"
     read -t1 response <&3 #$device
-  fi
-
-  count=$((count+1))
-  if [ $server = true ]; then
-      echo "$count: echoback"
-    if [ "x$response" = "xechostop" ]; then
-      message="server stop"
-      run=false
-    else
-      message=$response
-    fi
-    echo "Message recv: $response"
-  elif [ $atcmd = true ]; then
     echo "Message recv: $response"
     run=false    
   else
-    echo "Message recv: $response"
+    echo -e "\033[KMessage recv: $response"
     if [ "x$response" = "x$message" ]; then
-      echo "$count: PASSED!!"
-      run=false;  #remark this line to enable an infinite test
+	  failrate=$(bc<<<"scale=2;$failcount*100/$count")
+      echo -n "$count: PASSED!!"
+	  if [ $longtest = false ]; then
+        run=false
+	  fi
     else
-      echo "$count: FAILED!!"
-      run=false;
+	  failcount=$(($failcount+1))
+	  failrate=$(bc<<<"scale=2;$failcount*100/$count")
+      echo -n "$count: FAILED!!"
+      if [ $longtest = false ]; then
+	    run=false;
+	  fi
     fi
-    message=$count
-  fi
-
-  if [ $run = true ]; then
-    echo "Message sent: $message"
-    echo $message > $device
+	echo "Fail: $failcount ($failrate%)"
+    #message=$count
   fi
 done
 
