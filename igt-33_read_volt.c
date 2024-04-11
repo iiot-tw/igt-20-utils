b4c7661bcaa187a5c6e3e413ca7b5e442ea3d722
Raymond <32183809+iiot-tw@users.noreply.github.com>
2024-04-11 15:50:17 +0800

    Update igt-33_read_volt.c
    remove  incorrect information in the comment area

--- igt-33_read_volt.c
+++ igt-33_read_volt.c
@@ -1,9 +1,5 @@
 /* This script is created as an EXAMPLE for specific purpose on Neousys IGT series.
 
-   It works only when eth0 is configured in /etc/network/interfaces
-   Note that some network manager, such as connman, will ignore the configuration
-   in /etc/network/interface and make this script not work as expected.
-
    THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
    NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
